5bc8ea4a47a2182537915e092bb2816cd4f7a700
Funmi <folowookere@yahoo.com>
2018-05-10 22:37:41 +0000

    Lesson 3

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// Initial JS file
+// select book-list id
+var bookList = document.getElementById("book-list")
+
+
+// //select page-banner id
+var banner = document.getElementById("page-banner")
+
+
+// //select all elements with tag li
+var lis = document.getElementsByTagName("li");
+
+// //cycle through titles and show them in the console
+for (i = 0; i < titles.length; i++) {
+	console.log(titles[i])
+}
+
+//select all elements with title class
+var titles = document.getElementsByClassName("title");
+
+
+Array.from(titles).forEach(function(item) {
+ console.log(item);   
+})
