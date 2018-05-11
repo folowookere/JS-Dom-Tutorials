fd5fe47defb5130bbbf08fbcaa7223f36f6b7673
Funmi <folowookere@yahoo.com>
2018-05-11 00:33:53 +0000

    Lesson 5

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const wmf = document.querySelector("#book-list li:nth-child(2) .name");
-// console.log(wmf);
+var books = document.querySelectorAll("#book-list li .name");
 
-var books = document.querySelector("#book-list li .name");
-// console.log(books);
-
-books = document.querySelectorAll("#book-list li .name");
-// console.log(books);
-
-Array.from(books).forEach(function(book) {
-    console.log(book)
-    
+Array.from(books).forEach(function(book){
+    book.textContent += " (book title)";
 });
+
+const bookList = document.querySelector("#book-list");
+
+// Uncomment line 10 to see entire book list replaced
+// bookList.innerHTML = "<h2> Books and more books.</h2>";
+
+//Append text to html, will show at end of html
+bookList.innerHTML += "<p>This is how you add html</p>";
