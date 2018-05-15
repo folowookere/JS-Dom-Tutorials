e8d868e45d21c89fae636894839a14b97f4bb49a
Funmi <folowookere@yahoo.com>
2018-05-15 00:59:45 +0000

    Lesson 14

--- app.js
+++ app.js
@@ -34,6 +34,30 @@ addForm.addEventListener("submit", function(e){
     li.appendChild(deleteBtnSpan);
     list.appendChild(li);
     
+    //Attributes - Lesson 14 (all done in console)
     
-
+    //     var book = document.querySelector("li:first-child .name")
+    // undefined
+    // book
+    // <span data-cloud9-id="76" class="name">Name of the Wind</span>
+    // book.getAttribute("class")
+    // "name"
+    // book
+    // <span data-cloud9-id="76" class="name">Name of the Wind</span>
+    // book.setAttribute("class", "name-2")
+    // undefined
+    // book
+    // <span data-cloud9-id="76" class="name-2">Name of the Wind</span>
+    // book.hasAttribute("class")
+    // true
+    // book.hasAttribute("href")
+    // false
+    // book.removeAttribute("class");
+    // undefined
+    // book
+    // <span data-cloud9-id="76">Name of the Wind</span>
+    // book.setAttribute("class", "name")
+    // undefined
+    // book
+    // <span data-cloud9-id="76" class="name">Name of the Wind</span>
 });
