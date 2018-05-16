d77c584c6780d67592accf9585146c14136f3574
Funmi <folowookere@yahoo.com>
2018-05-15 23:14:20 -0400

    Lesson 15

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Delete books
+
 const list = document.querySelector("#book-list ul");
 //adding event listener to ul instead to delete books
 list.addEventListener("click", function(e){
@@ -14,50 +16,36 @@ const addForm = document.forms["add-book"]
 addForm.addEventListener("submit", function(e){
     e.preventDefault();
     const value = addForm.querySelector("input[type='text']").value;
-    
-    //Create elements
-    const li = document.createElement("li");
-    const bookNameSpan = document.createElement("span");
-    const deleteBtnSpan = document.createElement("span");
-    
-    //Add content
-    deleteBtnSpan.textContent = "delete";
-    bookNameSpan.textContent = value;
-    
-    //Add classes to elements
-    bookNameSpan.classList.add("name");
-    deleteBtnSpan.classList.add("delete");
-    
-    
-    //Append to the document
-    li.appendChild(bookNameSpan);
-    li.appendChild(deleteBtnSpan);
-    list.appendChild(li);
-    
-    //Attributes - Lesson 14 (all done in console)
-    
-    //     var book = document.querySelector("li:first-child .name")
-    // undefined
-    // book
-    // <span data-cloud9-id="76" class="name">Name of the Wind</span>
-    // book.getAttribute("class")
-    // "name"
-    // book
-    // <span data-cloud9-id="76" class="name">Name of the Wind</span>
-    // book.setAttribute("class", "name-2")
-    // undefined
-    // book
-    // <span data-cloud9-id="76" class="name-2">Name of the Wind</span>
-    // book.hasAttribute("class")
-    // true
-    // book.hasAttribute("href")
-    // false
-    // book.removeAttribute("class");
-    // undefined
-    // book
-    // <span data-cloud9-id="76">Name of the Wind</span>
-    // book.setAttribute("class", "name")
-    // undefined
-    // book
-    // <span data-cloud9-id="76" class="name">Name of the Wind</span>
+
+
+//Add content
+deleteBtnSpan.textContent = "delete";
+bookNameSpan.textContent = value;
+
+//Add classes to elements
+bookNameSpan.classList.add("name");
+deleteBtnSpan.classList.add("delete");
+
+
+//Append to the document
+li.appendChild(bookNameSpan);
+li.appendChild(deleteBtnSpan);
+list.appendChild(li);
+
+//Create elements
+const li = document.createElement("li");
+const bookNameSpan = document.createElement("span");
+const deleteBtnSpan = document.createElement("span");
+
+
+});
+
+// Hide books
+const hideBox = document.querySelector("#hide");
+hideBox.addEventListener ("change", function(e) {
+   if (hideBox.checked) {
+       list.style.display = "none";
+   } else {
+       list.style.display = "initial";
+   }
 });
