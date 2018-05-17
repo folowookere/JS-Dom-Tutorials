f5b2272b0310e9e8b3cd946a8aa09d94aa26a2e0
Funmi <folowookere@yahoo.com>
2018-05-17 01:46:23 -0400

    Lesson 17

--- app.js
+++ app.js
@@ -49,3 +49,20 @@ hideBox.addEventListener ("change", function(e) {
        list.style.display = "initial";
    }
 });
+
+//Tabbed Content
+const tabs = document.querySelector(".tabs");
+const panels = document.querySelectorAll(".panel");
+tabs.addEventListener("click", function(e){
+    if(e.target.tagName == "LI") {
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach(function(panel) {
+            if (panel == targetPanel) {
+                panel.classList.add("active");
+            } else {
+                panel.classList.remove("active");
+            }
+        });
+    }
+});
+
